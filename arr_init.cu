08e5e34cb01f84875dae74f83dffc148758cfda6
dipsankarb <dipsankarb@gmail.com>
2021-09-14 17:58:39 +0530

    update

--- arr_init.cu
+++ arr_init.cu
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#define N 1025
+#define N 1000
 
 __global__ void init(int *a, int *b) {
 	int id=threadIdx.x;
@@ -12,6 +12,21 @@ __global__ void assign(int *a){
 	a[id]=id;
 }
 
+__global__ void assign_new(int *a){
+	int id=threadIdx.x;
+	int i;
+
+	//NEW LOGIC
+	#pragma unroll
+	for(i=0; i<100; i++){
+		a[id*100+i]=id*100+i;
+	}
+
+	
+}
+
+
+
 int main() {
 	int a[N], i;
 
@@ -19,11 +34,13 @@ int main() {
 
 	cudaMalloc(&d_a, N*sizeof(int));
 
-	init<<<1,N>>>(d_a);
+	//init<<<1,N>>>(d_a);
 
 	cudaDeviceSynchronize();
 
-	assign<<<1,N>>>(d_a);
+	//assign<<<1,N>>>(d_a);
+	
+	assign_new<<<1,10>>>(d_a);
 
 
 	cudaMemcpy(a, d_a, sizeof(int)*N, cudaMemcpyDeviceToHost);
